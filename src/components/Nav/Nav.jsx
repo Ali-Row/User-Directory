537083fb9fac3f4e13b762f1ab9e4836c12481ea
Alistair <deadlyhardware@gmail.com>
2021-04-07 00:37:09 -0400

    :lipstick: Change searchbox color

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = ({ handleSearchChange }) => {
         <div className="container-fluid p-2 d-flex justify-content-start">
           <a href="/" className="navbar-brand mx-4 d-flex align-items-center"><i class="fab fa-superpowers mx-2"></i> User Directory</a>
           <form className="d-flex align-items-center">
-          <input className="form-control me-2 rounded-pill" type="search" placeholder="Search users..." onChange={e => handleSearchChange(e)} aria-label="Search" /> 
+          <input className="form-control me-2 rounded-pill search" type="search" placeholder="Search users..." onChange={e => handleSearchChange(e)} aria-label="Search" /> 
           <i class="fas fa-search"></i>
           </form>
         </div>
